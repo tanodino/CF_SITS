8c3af22387b71000641496fd7a5b4dd02f7f8637
Thalita Drumond <thalitafdrumond@gmail.com>
2023-03-15 18:37:08 +0000

    formatting and documenting

--- competitor_knn.py
+++ competitor_knn.py
@@ -9,8 +9,8 @@ author: tdrumond
 date: 2023-03-13 16:39:09
 
 """
-from pathlib import Path
 import os
+from pathlib import Path
 import numpy as np
 import logging
 
@@ -22,50 +22,12 @@ from cfsits_tools.data import loadAllDataNpy, VALID_SPLITS, npyData2DataLoader
 from cfsits_tools.model import S2Classif
 from cfsits_tools.utils import loadModel, savePklModel, loadPklModel, torchModelPredict
 
-class KNeighborsCounterfactual(OriginalKNeighborsCounterfactual):
-    def _validate_estimator(self, estimator, allow_3d=False):
-        """override estimator validation to allow using our model"""
-        return estimator
-
-
-class ModelWrapper():
-    """Wrap our models in order to provide the necessary interface for the nn counterfactual """
-
-    def __init__(
-            self, 
-            model, 
-            n_neighbors:int=1):
-        self._model = model
-
-        # n_neigbors is used in two situations:
-        # 1) to compute n_clusters = n_samples// n_neighbors
-        # 2) to compute the majority threshold: 1 + n_neighbors//n_classes
-        self.n_neighbors = n_neighbors
-        self._fit_X = None
-        self._y = None
-        self.n_features_in_ = None
-        self.classes_ = None
-
-    def fit(self, X, y):
-        # wildboar's KNeighborsCounterfactual expects estimator
-        # to have attributes _fit_X, _y, classes_ and n_features_in_
-        self._fit_X = np.squeeze(X)
-        # XXX maybe need to adapt _y for multiclass? Folowing sklearn's implemntation at
-        # https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/neighbors/_base.py#L451
-
-        # _y should be the y_pred for the samples in _fit_X
-        X_dl = npyData2DataLoader(X, batch_size=64)
-        self._y = torchModelPredict(self._model, X_dl)
-        self.n_features_in_ = X.shape[1]
-        self.classes_ = np.unique(y)
-        
-
 
 def trainCfModel(args):
     # Load data
     fullData = loadAllDataNpy(year=args.year)
 
-    # load classification model 
+    # load classification model
     model = S2Classif(fullData["n_classes"])
     loadModel(model, file_path=args.model_name)
 
@@ -84,16 +46,16 @@ def trainCfModel(args):
         cfModel.fit(clfModel)
     logging.info("Cf model trained")
 
-    savePklModel(cfModel, args.noiser_name)
+    savePklModel(cfModel, getNoiserName(args), args.model_dir)
     logging.info("Cf model saved")
 
-def predictCfSamples(args):
 
+def predictCfSamples(args):
     # Load data
     fullData = loadAllDataNpy(year=args.year)
 
     # load CF model
-    cfModel = loadPklModel(args.noiser_name)
+    cfModel = loadPklModel(getNoiserName(args), args.model_dir)
 
     # produce CF samples using the method explain
     # explain takes X, y as input
@@ -101,14 +63,13 @@ def predictCfSamples(args):
     # here y should contain the "destination class"
     # into which the CF explainer is going to try to push X
     # I can generate CFs to all possible classes
-    
+
     split = args.split
     X, y = fullData[split]
     X = np.squeeze(X)
-    n_samples = X.shape[0]
     for src_label in fullData["classes"]:
         logging.info(f"Building Cfs from class {src_label}...")
-        X_src = X[y==src_label]
+        X_src = X[y == src_label]
         for dst_label in fullData['classes']:
             if dst_label != src_label:
                 logging.info(f"Building Cfs to class {dst_label}...")
@@ -123,35 +84,92 @@ def predictCfSamples(args):
                 output_fpath = Path(args.out_path, output_fname)
                 os.makedirs(output_fpath.parent, exist_ok=True)
                 np.save(output_fpath, X_cfs)
-                logging.info(f"Cfs from class {int(src_label)} saved to {output_fname}")
+                logging.info(
+                    f"Cfs from class {int(src_label)} saved to {output_fname}")
+
+
+class KNeighborsCounterfactual(OriginalKNeighborsCounterfactual):
+    def _validate_estimator(self, estimator, allow_3d=False):
+        """override estimator validation to allow using our model"""
+        return estimator
+
+
+class ModelWrapper():
+    """Wrap our models in order to provide the necessary interface for the nn counterfactual """
+
+    def __init__(
+            self,
+            model,
+            n_neighbors: int = 1):
+        self._model = model
+
+        # n_neigbors is used in two situations:
+        # 1) to compute n_clusters = n_samples// n_neighbors
+        # 2) to compute the majority threshold: 1 + n_neighbors//n_classes
+        self.n_neighbors = n_neighbors
+        self._fit_X = None
+        self._y = None
+        self.n_features_in_ = None
+        self.classes_ = None
+
+    def fit(self, X, y):
+        # wildboar's KNeighborsCounterfactual expects estimator
+        # to have attributes _fit_X, _y, classes_ and n_features_in_
+        self._fit_X = np.squeeze(X)
+        self.n_features_in_ = self._fit_X.shape[1]
+        self.classes_ = np.unique(y)
+
+        # _y should be the y_pred for the samples in _fit_X
+        X_dl = npyData2DataLoader(X, batch_size=64)
+        self._y = torchModelPredict(self._model, X_dl)
+        # XXX maybe need to adapt _y for multiclass? Folowing sklearn's implemntation at
+        # https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/neighbors/_base.py#L451
+
+
+def getNoiserName(args):
+    return args.noiser_name.replace(
+        "knn",
+        f"{args.n_neighbors}nn")
 
 
 if __name__ == "__main__":
+    LOG_DIR = os.path.join('logs', os.path.basename(
+        os.path.splitext(__file__)[0]))
 
     parser = getBasicParser()
     parser.set_defaults(noiser_name='noiser_knn')
+    parser.add_argument(
+        "-k", "--n-neighbors",
+        help="number of neighbors passed to the NN counterfactual model",
+        default=1
+    )
+
+    parser.add_argument(
+        "--model-dir",
+        help=f"Dir where noiser model is dumped/loaded. Defaults to {LOG_DIR}.",
+        default=Path(LOG_DIR),
+        type=Path
+    )
 
     subparsers = parser.add_subparsers(
         dest='subcommand',
         required=True)
+
     # train command parsing
     train_cmd = subparsers.add_parser(
         'train',
-        description='trains a knn cf model'
-    )
-    train_cmd.add_argument(
-        "-k", "--n-neighbors",
-        default=1
+        help='trains a knn cf model'
     )
 
     # pred command parsing
     pred_cmd = subparsers.add_parser(
         'pred',
-        description='preds CF samples from previously trained CF model'
+        help='preds CF samples from previously trained CF model'
     )
     pred_cmd.add_argument(
         "--out-path",
-        default="logs/knn_CFs"
+        default=Path(LOG_DIR, "cf_data"),
+        type=Path
     )
     pred_cmd.add_argument(
         "--split",
@@ -168,4 +186,3 @@ if __name__ == "__main__":
         trainCfModel(args)
     elif args.subcommand == 'pred':
         predictCfSamples(args)
-
