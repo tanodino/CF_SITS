aeccd84cf549fa2f7afdb2eb85ec25c91ec05570
Thalita Drumond <thalita.drumond@epf.fr>
2023-06-30 16:25:10 +0000

    adapt to new cli + support new feats now supports the choice of datasets, clf arch, etc. basically now it supports as much as main noiser and main classif

--- ExtractCF.py
+++ ExtractCF.py
@@ -11,136 +11,159 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import NearestNeighbors
+from cfsits_tools import log
+from cfsits_tools import cli
+from cfsits_tools import utils
+from cfsits_tools import viz
+from cfsits_tools import metrics
 
 from cfsits_tools.cli import getBasicParser
 from cfsits_tools.metrics import compactness, metricsReport, plausibility, proximity, stability, validity
-from cfsits_tools.model import MLPClassif, MLPBranch, Noiser, Discr, S2Classif
+from cfsits_tools.model import Inception, MLPClassif, MLPBranch, Noiser, Discr, S2Classif
 from cfsits_tools.utils import ClfPrediction, getCurrentDevice, loadWeights, predictionAndCF, setFreeDevice
 from cfsits_tools.viz import printConfMatrix, printSomeMetrics
-from cfsits_tools.data import loadAllDataNpy, loadSplitNpy, npyData2DataLoader, VALID_SPLITS
+from cfsits_tools.data import DEFAULT_DATA_DIR, load_UCR_dataset, loadAllDataNpy, loadSplitNpy, npyData2DataLoader, VALID_SPLITS
 from cfsits_tools.viz import produceResults
 
 
-def main(args):
-    logging.info(f"Data split: {args.split}")
+MODEL_DIR = utils.DEFAULT_MODEL_DIR
+DATA_DIR = DEFAULT_DATA_DIR
 
-    # Load data
-    fullData = loadAllDataNpy(year=args.year, squeeze=True)
-    n_classes = fullData['n_classes']
-    X, y_true = fullData[args.split]
-    # X, y_true = loadSplitNpy(args.split, args.year)
-    dataloader = npyData2DataLoader(X, y_true, batch_size=2048)
 
-    # globally set a free gpu as device (if available)
-    setFreeDevice()
 
-    # Load model
-    logging.info('Loading classifier')
-    model = S2Classif(n_class=len(np.unique(y_true)), dropout_rate=.5)
-    model.to(getCurrentDevice())
-    loadWeights(model, args.model_name)
+def launchInference(args):
+    # Load training data and 
+    # Load data from requested split
+    logger.info(f"Loading dataset {args.dataset} - {args.split} split")
+    if args.dataset == "koumbia":
+        x_train, y_train = loadSplitNpy(
+            'train', data_path=DATA_DIR, year=args.year, squeeze=True)
+        X, y_true = loadSplitNpy(
+            args.split, data_path=DATA_DIR, year=args.year, squeeze=True)
+    else:  # load UCR dataset
+        x_train, y_train = load_UCR_dataset(args.dataset, split='train')
 
-    # load noiser
-    logging.info('Loading noiser')
+        X, y_true = load_UCR_dataset(args.dataset, split=args.split)
+
+    n_classes = len(np.unique(y_train))
+    logger.info(f'x_train shape: {x_train.shape}')
+    logger.info(f'X shape: {X.shape}')
+
+
+    # Classification model
+    if args.model_arch == 'TempCNN':
+        model = S2Classif(n_classes)
+    elif args.model_arch == 'Inception':
+        model = Inception(n_classes)
+    elif args.model_arch == 'MLP':
+        model = MLPClassif(n_classes)
+
+    # Load classification model weights
+    logger.info('Loading classifier')
+    model_params = utils.loadWeightsAndParams(model, args.model_name)
+    logger.info(f'Classifier params: {model_params}')
+
+
+    # noiser model
     n_timestamps = X.shape[-1]
-    noiser = Noiser(n_timestamps, .3, shrink=args.shrink)
-    noiser.to(getCurrentDevice())
-    loadWeights(noiser, args.noiser_name)
+    noiser = Noiser(
+        out_dim=n_timestamps,
+        shrink=args.shrink,
+        base_arch=args.noiser_arch)
 
-    # CF data of the chosen split
-    y_pred, y_predCF, dataCF, noiseCF = predictionAndCF(
-        model, noiser, dataloader)
+    # Load noiser model weights
+    logger.info('Loading noiser')
+    noiser_params = utils.loadWeightsAndParams(noiser, args.noiser_name)
+    logger.info(f'Noiser params: {noiser_params}')
+
+
+    # device setup
+    utils.setFreeDevice()
+    device = utils.getCurrentDevice()
+    model.to(device)
+    noiser.to(device)
+
+    # setup dataloaders
+    train_dataloader = npyData2DataLoader(x_train, batch_size=2048)
+    dataloader_y_true = npyData2DataLoader(X, y_true, batch_size=2048)
+
+    # compute CF data of the chosen split
+    y_pred, y_predCF, dataCF, noiseCF = utils.predictionAndCF(
+        model, noiser, dataloader_y_true)
 
     # print confusion matrix for somples correctly predicted
     correct_idx = y_true == y_pred
     cm = confusion_matrix(y_pred[correct_idx], y_predCF[correct_idx])
-    printConfMatrix(cm)
+    viz.printConfMatrix(cm)
 
     if args.do_plots:
         # write plots and tables
         # Prepare output path
-        output_folder = Path(
-            args.img_path,
-            f"{args.model_name}_{args.noiser_name}",
-            f"{args.split}")
+        output_folder = IMG_PATH
+        # output_folder = Path(
+        #     IMG_PATH,
+        #     # f"{args.model_name}_{args.noiser_name}",
+        #     f"{args.split}")
         os.makedirs(output_folder, exist_ok=True)
 
-        produceResults(args.split, output_folder, y_true,
-                    y_pred, y_predCF, dataCF, noiseCF)
+        viz.produceResults(args.split, output_folder, y_true,
+                           y_pred, y_predCF, dataCF, noiseCF)
 
         # TSNE
         # plotTSNE(y_true, pred, predCF, noise_CF, output_folder,
         # random_state=args.seed)
 
 
-
     # CF data of train is needed for stability computation
-    _, _, trainCF, _ = predictionAndCF(
-        model, noiser, npyData2DataLoader(fullData['train'].X, batch_size=2048))
+    _, _, trainCF, _ = predictionAndCF(model, noiser, train_dataloader)
 
     # Compute predictions for train data
-    y_pred_train = ClfPrediction(
-        model, npyData2DataLoader(fullData['train'].X, batch_size=2048))
-    logging.info(f"Metrics computed on {args.split} data")
-    metricsReport(
+    y_pred_train = ClfPrediction(model, train_dataloader)
+
+
+    logger.info(f"Metrics computed on {args.split} data")
+    metrics.metricsReport(
         X=X, Xcf=dataCF.squeeze(),
         y_pred_cf=y_predCF,
-        X_train=fullData['train'].X, 
+        X_train=x_train,
         y_pred_train=y_pred_train,
-        Xcf_train=trainCF.squeeze(), 
-        ifX=fullData['train'].X, k=args.n_neighbors, model=model)
+        Xcf_train=trainCF.squeeze(),
+        ifX=x_train, k=args.stability_k, model=model)
 
 
 if __name__ == "__main__":
-    LOG_DIR = os.path.join('logs', os.path.basename(
-        os.path.splitext(__file__)[0]))
-    os.makedirs(LOG_DIR, exist_ok=True)
-    parser = getBasicParser()
+    parser = cli.getBasicParser()
+    parser = cli.addClfLoadArguments(parser)
+    parser = cli.addNoiserLoadArguments(parser)
+    parser = cli.addInferenceParams(parser)
     parser.add_argument(
-        '--img-path',
-        default='img',
-        help='Directory in which images are saved'
-    )
-    parser.add_argument(
-        '--split',
-        choices=VALID_SPLITS,
-        default='test',
-        help='Data partition used to compute results.'
-    )
-    parser.add_argument(
-        '-k','--n-neighbors',
+        '--stability-k',
         type=int,
-        default=5
+        default=5,
+        help='Number of neighbors used to compute stability metric.'
+        'Used only during evaluation (mode=pred).'
     )
+
     parser.add_argument(
         '--do-plots',
         action='store_true',
         default=False,
-        help='Runs plotting functions and writes results to IMG_PATH'
-    )    
-    parser.add_argument(
-        '--shrink',
-        action='store_true',
-        default=False,
-        help='To be used when the Noiser was trained with shrink=True'
+        help='Runs plotting functions and writes results to logdir.'
     )
-    parser.add_argument(
-        '--logfile',
-        default='log.txt',
-        help='Choose logging file name'
-    )
+
     args = parser.parse_args()
 
-    logging.basicConfig(
-        level=args.log_level,
-        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
-        datefmt='%Y/%m/%d %H:%M:%S',
-        handlers=[
-            logging.FileHandler(
-                Path(LOG_DIR, args.logfile), mode="w"
-            ),
-            logging.StreamHandler(sys.stdout)
-        ]
-    )
-    main(args)
+    logger = log.setupLogger(__file__, parser)
+
+    logger.info(f"Setting manual seed={args.seed} for reproducibility")
+    utils.setSeed(args.seed)
+
+    # Create img dir within log dir if needed
+    IMG_PATH = os.path.join(log.getLogdir(), 'img')
+    if args.do_plots:
+        os.makedirs(IMG_PATH, exist_ok=True)
+
+    launchInference(args)
+
+    # main(args)
 
